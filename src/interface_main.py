b73e97af10a262a85a6e2ef388a3a3f5e91eb92d
Shane Walsh <shane.walsh.3@ucdconnect.ie>
2020-10-22 22:29:17 +0100

    Accept relevant inputs

--- src/interface_main.py
+++ src/interface_main.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
+from PyQt5 import QtGui as qtg
 
 from user_interface import Ui_Form
 
@@ -11,11 +12,34 @@ class UserInterface(qtw.QWidget):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
 
+        # Validators
+        self.onlyInt = qtg.QIntValidator()
+        self.onlyFloat = qtg.QDoubleValidator()
+        self.ui.mortgage_input.setValidator(self.onlyInt)
+        self.ui.initial_interest_input.setValidator(self.onlyFloat)
+        self.ui.base_full_term_input.setValidator(self.onlyInt)
+
+        # Connections
+        self.ui.run_button.clicked.connect(self.run)
+
+    def run(self):
+        try:
+            mortgage_value = int(self.ui.mortgage_input.text())
+            interest_rate = float(self.ui.initial_interest_input.text()) / (100 * 12)
+            term_length = int(self.ui.base_full_term_input.text())
+        except ValueError:
+            qtw.QMessageBox.critical(
+                self, 'Warning', 'Please ensure all information is put in correctly'
+            )
+            return
+
+        qtw.QMessageBox.information(self, 'Success', 'Ran successfully')
+
 
 if __name__ == '__main__':
-     app = qtw.QApplication([])
+    app = qtw.QApplication([])
 
-     widget = UserInterface()
-     widget.show()
+    widget = UserInterface()
+    widget.show()
 
-     app.exec_()
+    app.exec_()
